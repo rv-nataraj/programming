6159f543873f1c9160df5bbebf04f9d44e22f1e3
rv-nataraj <rv.nataraj@gmail.com>
2021-08-28 16:28:59 +0000

    squarepattern

---
+++ c/squarepattern.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    for(int row=1; row<=9; row++)
+    {
+        for(int col=1; col<=9; col++)
+        {
+            printf("# ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
