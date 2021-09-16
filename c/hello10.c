ff6052576e602a281a2d926e9d75cb3f4901a226
rv-nataraj <rv.nataraj@gmail.com>
2021-09-16 17:06:43 +0000

    hello10

---
+++ c/hello10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	printf("\nHello");
+	printf("\nHello");
+	printf("\nHello");
+	printf("\nHello");
+	printf("\nHello");
+	printf("\nHello");printf("\nHello");printf("\nHello");printf("\nHello");printf("\nHello");
+
+	for(int i=1; i<=10; i++)
+	{
+		printf("\nHello");
+	}
+	return 0;
+}
