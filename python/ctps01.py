ba8b2f53d7a7ff7b5fcaa6ee2fc5f1dd6679a62d
rv-nataraj <rv.nataraj@gmail.com>
2021-11-12 09:08:06 +0000

    ctps

---
+++ python/ctps01.py
@@ -0,0 +1,4 @@
+train_length=input("Enter the length of the train:")
+bridge_length=input("Enter the length of the bridge:")
+print("The distance travelled by the train to completely cross the bridge is",int(train_length)+int(bridge_length))
+
