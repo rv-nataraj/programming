e7e5d9d89047ffb0f55ff3caeecc0a6f34af0d49
rv-nataraj <rv.nataraj@gmail.com>
2021-12-13 06:45:47 +0000

    gretest2

---
+++ c/greatet2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a=40;
+    int b=60;
+    if (a>b)
+    {
+        printf("a is greater");
+    }
+    else 
+    {
+        printf("b is greater");
+    }
+    return 0;
+}
